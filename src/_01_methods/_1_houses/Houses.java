c7e8df6df53d47abcfcb6f2ca56a30fe6e12e009
root <root@b89b1d3bee5f>
2023-06-21 01:26:18 +0000

    Houses, On Level 1!

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,66 @@
 package _01_methods._1_houses;
+
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
-		
+	Robot rob = new Robot();
+	Random ran = new Random();
+
 	public void run() {
 		// Check the recipe to find out what code to put here
+		rob.setSpeed(50);
+		rob.turn(-90);
+		rob.move(375);
+		rob.turn(90);
+		rob.penDown();
+		for (int i = 0; i < 10; i++) {
+			int num = ran.nextInt(3);
+			if (num == 0) {
+				house("small");
+			}
+			if (num == 1) {
+				house("medium");
+			}
+			if (num == 2) {
+				house("large");
+			}
+		}
+
+	}
+
+	void house(String sheight) {
+		int height = 0;
+
+		if (sheight.equals("small")) {
+			height = 60;
+		}
+
+		if (sheight.equals("medium")) {
+			height = 120;
+		}
+
+		if (sheight.equals("large")) {
+			height = 250;
+		}
+		
+		void house(String color) {
+			int color = 0;
+			
+			if (color.equals)
+		}
+		rob.move(height);
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(90);
+		rob.move(height);
+		rob.turn(-90);
+		rob.move(25);
+		rob.turn(-90);
 	}
 }
